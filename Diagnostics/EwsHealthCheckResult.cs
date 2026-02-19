1a60847cd27f72a92b9c8bd05f92d8fef793d318
Sergey Limonov <sergey.limonov@gmail.com>
2026-02-19 12:57:46 +0300

    переключение на .NET 10

--- Diagnostics/EwsHealthCheckResult.cs
+++ Diagnostics/EwsHealthCheckResult.cs
@@ -6,6 +6,6 @@ public sealed class EwsHealthCheckResult
 {
 	public bool Success { get; init; }
 	public EwsHealthStatus Status { get; init; }
-	public string? Message { get; init; }
-	public Uri? Endpoint { get; init; }
+	public string Message { get; init; } 
+	public Uri Endpoint { get; init; }
 }
